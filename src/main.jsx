1c597fb064e0b4e952e04c7b3d4dce237dca93c3
szm0227 <szm0227@auburn.edu>
2026-03-03 22:31:29 -0600

    fixing bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App";
+import App from "./App.jsx";
 import { AuthProvider } from "./auth.jsx";
 import "./styles.css";
 
