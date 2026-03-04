9238963ee6cd3eca452b15474e3a3012ce49a975
szm0227 <szm0227@auburn.edu>
2026-03-03 22:41:20 -0600

    updating route

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/ipl-picks/" : "/",
-
-  plugins: [
-    react({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
-  ],
+  base: mode === "production" ? "/iplpicks/" : "/",
+  plugins: [react()],
 }));
